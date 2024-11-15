4e3f642fa2e187d1f8b783be574e3e0aa0cc300c
kalra-50 <54609831+kalra-50@users.noreply.github.com>
2024-11-15 16:24:35 -0500

    modified for ROI of intersection

--- src/+helper/getGridParameters.m
+++ src/+helper/getGridParameters.m
@@ -4,18 +4,20 @@ function gridParams = getGridParameters()
 %
 % Copyright 2021 The MathWorks, Inc.
 
-    xMin = -25.0;     % Minimum value along X-axis.
-    xMax = 25.0;      % Maximum value along X-axis.
-    yMin = 0.0;       % Minimum value along Y-axis.
-    yMax = 50.0;      % Maximum value along Y-axis.
-    zMin = -7.0;      % Minimum value along Z-axis.
-    zMax = 15.0;      % Maximum value along Z-axis.
+    xMin = -30.0;     % Minimum value along X-axis.
+    xMax = 62.0;      % Maximum value along X-axis.
+    yMin = 0;         % Minimum value along Y-axsis.
+    yMax = 105.0;      % Maximum value along Y-axis.l
+    zMin = -7.0;      % Minimum value along Z-axis. % Original ROI network X [-25 25], Y: [0 50]
+    zMax = 1.0;      % Maximum value along Z-axis.  %Original ROI ISC : X: [-30, 62], Y: [-50, 55],
+    f_x  = (xMax - xMin)/50;
+    f_y = (yMax - yMin)/50;
 
     pcRange = [xMin xMax yMin yMax zMin zMax];
 
     % Define the dimensions for the pseudo-image.
-    bevHeight = 608;
-    bevWidth = 608;
+    bevHeight = round(f_x*608);
+    bevWidth =  round(f_y*608);
 
     % Find grid resolution.
     gridW = (pcRange(4) - pcRange(3))/bevWidth;
